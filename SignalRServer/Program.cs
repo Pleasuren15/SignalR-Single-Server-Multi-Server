50c111c61dcabd3059fbf17828530e1300200bfe
Pleasure Ndhlovu <pleasuren15@gmail.com>
2025-10-01 09:55:16 +0200

    wip: CORS updated

--- SignalRServer/Program.cs
+++ SignalRServer/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+        policy.WithOrigins("http://localhost:8080", "http://127.0.0.1:8081")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
